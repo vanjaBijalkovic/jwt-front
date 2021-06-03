ec88d7db6d84d4f7d3cc623934215bfd2d20dfca
Bijalkovic <bijalkovicvanja@gmail.com>
2021-06-03 21:16:48 +0200

    Added auth guard and added login component

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import LoginComponent from '../views/LoginComponent'
+
+import { globalAuthGuard } from '../guards/auth.guard'; 
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/about',
+    meta: { authRequired: true }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginComponent
   },
   {
     path: '/about',
     name: 'About',
+    meta: { authRequired: true },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -25,5 +33,7 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach(globalAuthGuard);
 
 export default router
---
+++ src/views/LoginComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <b-form @submit.stop.prevent="login()">
+        <b-form-group
+          label="Email Adress:"
+          label-for="email"
+          description="Email adress"
+        >
+          <b-form-input
+            id="email"
+            v-model="user.email"
+            type="email"
+            required
+            placeholder="Enter email"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group
+          label="Password:"
+          label-for="password"
+          description="Password"
+        >
+          <b-form-input
+            id="password"
+            v-model="user.password"
+            type="password"
+            required
+            placeholder="Enter password"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-button type="submit" variant="dark">Submit</b-button>
+      </b-form>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: "LoginComponent",
+  data() {
+    return {
+      user: {},
+    };
+  },
+  components: {},
+  methods: {
+    login() {
+      console.log(this.user)
+    }
+  }
+};
+</script>
